5fec6bdc6d97c761335f787c7e2f832cd123cb09
DisputOS <disput.ai.project@gmail.com>
2025-05-02 23:49:36 +0200

    Update route.ts

--- app/api/generatePdf/route.ts
+++ app/api/generatePdf/route.ts
@@ -1,10 +1,7 @@
 export const runtime = 'nodejs';
 
-
 import { NextRequest, NextResponse } from 'next/server';
 import { generatePDF } from '@/utils/pdfGenerator';
-
-export const runtime = 'edge'; // можно и вовсе не указывать
 
 export async function POST(req: NextRequest) {
   const { disputeId, templateText } = await req.json();
