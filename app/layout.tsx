e0afde8f8e9e2d3fcb482b3ac77cdc814985e2d9
DisputOS <disput.ai.project@gmail.com>
2025-05-05 20:12:10 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,16 +11,3 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   )
 }
-
-import { Toaster } from 'sonner';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Toaster richColors />
-        {children}
-      </body>
-    </html>
-  );
-}
