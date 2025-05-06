04c4db433f8ff8fc80a681d98535a175f1dd2978
DisputOS <disput.ai.project@gmail.com>
2025-05-06 15:06:02 +0200

    Create GlowingBorder.tsx

---
+++ components/GlowingBorder.tsx
@@ -0,0 +1,7 @@
+// components/GlowingBorder.tsx
+import React from "react";
+import styles from "./GlowingBorder.module.css";
+
+export default function GlowingBorder() {
+  return <div className={styles.glow}></div>;
+}
