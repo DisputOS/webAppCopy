b5574f9f5b2d10b2c404236f8bc69a55f1ec7225
DisputOS <disput.ai.project@gmail.com>
2025-05-06 17:22:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link rel="icon" type="image/png" href="/mainicon.png" />
         <link rel="apple-touch-icon" href="/mainicon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
+        
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+        <meta name="theme-color" content="#1a202c" />
         {/* Можно добавить ещё OpenGraph, favicon.ico и пр. */}
       </head>
       <body className="min-h-screen bg-gray-50 relative">
