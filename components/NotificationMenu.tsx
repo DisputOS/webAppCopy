ac21143ed305f46ea548ab3a3fd149da638d0db2
DisputOS <disput.ai.project@gmail.com>
2025-05-10 18:58:40 +0200

    Update NotificationMenu.tsx

--- components/NotificationMenu.tsx
+++ components/NotificationMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Bell, Check, Loader2 } from "lucide-react";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { useNotifications } from "@/hooks/useNotifications";
 import clsx from "clsx";
 
