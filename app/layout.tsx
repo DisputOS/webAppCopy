1b48f57d7812c08eba79c0d99e40ffe5e878ff6f
DisputOS <disput.ai.project@gmail.com>
2025-05-11 19:33:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <title>DisputApp</title>
         <link rel="icon" type="image/png" href="/mainicon.png" />
-        <link rel="manifest" href="/manifest.json" />
+      <link rel="manifest" href="/manifest.json">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="theme-color" content="#1a202c"><!-- Android/Chrome -->
+
         <link rel="apple-touch-icon" sizes="57x57" href="/icons/icon-57x57.png" />
 <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
 <link rel="apple-touch-icon" sizes="76x76" href="/icons/icon-76x76.png" />
