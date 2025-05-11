ef825e6f85e198fdde3b461bd0e401009153f576
DisputOS <disput.ai.project@gmail.com>
2025-05-12 00:18:59 +0200

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -103,7 +103,7 @@ export default function SettingsPage() {
   return (
     <>
       <Header />
-    <main className="min-h-screen">
+    <main className="min-h-screen  p-6 space-y-6">
       <div className="max-w-3xl mx-auto space-y-10">
         <h1 className="text-3xl font-bold">My Settings</h1>
 
