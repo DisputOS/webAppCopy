70cf2ae91f271b7802131c151509315ef94bc0ac
DisputOS <disput.ai.project@gmail.com>
2025-05-12 19:33:53 +0200

    Update GlowyBackground.tsx

--- components/GlowyBackground.tsx
+++ components/GlowyBackground.tsx
@@ -3,10 +3,27 @@
 
 export default function GlowyBackground() {
  return (
-    <div className="blobs-container">
-      <div className="blob blob--orange" />
-      <div className="blob blob--blue" />
-      <div className="blob blob--cyan" />
-    </div>
+    <>
+      <svg width="0" height="0" style={{ position: 'absolute' }} aria-hidden>
+        <filter id="wavy">
+          <feTurbulence
+            type="turbulence"
+            baseFrequency="0.005 0.01"
+            numOctaves="2"
+            seed="4"
+            result="turb"
+          >
+            <animate
+              attributeName="baseFrequency"
+              values="0.005 0.01;0.007 0.012;0.005 0.01"
+              dur="12s"
+              repeatCount="indefinite"
+            />
+          </feTurbulence>
+          <feDisplacementMap in="SourceGraphic" in2="turb" scale="25" xChannelSelector="R" yChannelSelector="G" />
+        </filter>
+      </svg>
+      <div className="animated-bg" />
+    </>
   );
 }
