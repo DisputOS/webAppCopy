cb3f082433e753e4d7b6166ab3c626f93681d3ac
Vladyslav <vladislav.danyliuk@gmail.com>
2025-05-13 20:35:54 +0200

    123

--- components/TabbedDisputeDetail.tsx
+++ components/TabbedDisputeDetail.tsx
@@ -112,9 +112,13 @@ export default function TabbedDisputeDetail({ dispute, proofs, proofCount }: Pro
       <div {...handlers} ref={containerRef} className="swipe-container relative overflow-hidden">
         {/* Corner highlights */}
         <div
-          onClick={() => setActiveIdx(i => Math.max(i - 1, 0))}
-          className="absolute left-0 top-1/2 -translate-y-1/2 h-[33%] w-[20%] rounded-r-2xl pointer-events-auto z-10 bg-[radial-gradient(circle_at_20%_50%25,_rgba(255,255,255,0.5),_rgba(255,255,255,0.2),_transparent)] animate-[rgbFade_8s_ease-in-out_infinite_alternate]"
-        />
+  onClick={() => setActiveIdx(i => Math.max(i - 1, 0))}
+  className="highlight-left"
+/>
+<div
+  onClick={() => setActiveIdx(i => Math.min(i + 1, TABS.length - 1))}
+  className="highlight-right"
+/>
         <div
           onClick={() => setActiveIdx(i => Math.min(i + 1, TABS.length - 1))}
           className="absolute right-0 top-1/2 -translate-y-1/2 h-[33%] w-[20%] rounded-l-2xl pointer-events-auto z-10 bg-[radial-gradient(circle_at_80%_50%25,_rgba(255,255,255,0.5),_rgba(255,255,255,0.2),_transparent)] animate-[rgbFade_8s_ease-in-out_infinite_alternate]"
