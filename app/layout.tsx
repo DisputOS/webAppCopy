51637774074c3e7bda5f6161b2c7bc6fe4bc0b23
Vladyslav <vladislav.danyliuk@gmail.com>
2025-05-17 21:27:46 +0200

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <title>DisputApp</title>
 
         {/* Manifest & icons */}
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" href="/icons/icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/icons/icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/icons/icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/icons/icon-76x76.png" />
+        {/* Manifest & icons */}
+        <link rel="manifest"            href="/manifest.json" />
+        <link rel="icon"     type="image/png" href="/icons/icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="57x57"  href="/icons/icon-57x57.png"  />
+        <link rel="apple-touch-icon" sizes="72x72"  href="/icons/icon-72x72.png"  />
+        <link rel="apple-touch-icon" sizes="76x76"  href="/icons/icon-76x76.png"  />
         <link rel="apple-touch-icon" sizes="120x120" href="/icons/icon-120x120.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-167x167.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-180x180.png" />
+
 
         {/* PWA / iOS handling */}
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,viewport-fit=cover" />
